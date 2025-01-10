022a5b1a0f7b79dd96ad0d4045b89eb62317b4ee
13urn0ut <nitrojunky@gmail.com>
2025-01-10 13:46:53 +0200

    Add workout model

---
+++ models/workoutModel.js
@@ -0,0 +1,10 @@
+class Workout {
+  constructor(id, name, date, userId) {
+    this.id = id;
+    this.name = name;
+    this.date = date;
+    this.user_id = userId;
+  }
+}
+
+module.exports = Workout;
