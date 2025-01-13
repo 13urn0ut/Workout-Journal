8ff45dca6fb5436913e2751ffc3cd1f0a8a6f5f4
13urn0ut <nitrojunky@gmail.com>
2025-01-13 20:58:22 +0200

    Add login to authRouter

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { createUser } = require("../controllers/userController");
+const { createUser, loginUser } = require("../controllers/userController");
 const authRouter = express.Router();
 
 authRouter.route("/register").post(createUser);
-authRouter.route("/login").post();
+authRouter.route("/login").post(loginUser);
 
 module.exports = authRouter;
