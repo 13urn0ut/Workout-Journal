0a39f41d6867b2c1dba145a807b98957dcca7fc2
13urn0ut <arturas.mickevicius@stud.techin.lt>
2025-01-14 14:15:06 +0400

    Add getUserById to userRouter

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {
   getAllUsers,
   // createUser,
-  // getUserById,
+  getUserById,
   // getUserByUsername,
 } = require("../controllers/userController");
 // const { addWorkout } = require("../controllers/workoutController");
@@ -11,7 +11,7 @@ const userRouter = express.Router();
 userRouter.route("/").get(getAllUsers);
 // .post(createUser);
 
-// userRouter.route(`/:id`).get(getUserById);
+userRouter.route(`/:id`).get(getUserById);
 // userRouter.route(`/:username`).get(getUserByUsername);
 
 // userRouter.route(`/:id/workouts`).post(addWorkout);
