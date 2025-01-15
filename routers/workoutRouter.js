2ce8858637f51621d998667d5f9ba866280eceeb
13urn0ut <arturas.mickevicius@stud.techin.lt>
2025-01-15 10:50:52 +0400

    Add getAllWorkouts to workoutRouter

--- routers/workoutRouter.js
+++ routers/workoutRouter.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const {
   getAllWorkouts,
-  getWorkoutById,
-  editWorkout,
-  deleteWorkout,
+  // getWorkoutById,
+  // editWorkout,
+  // deleteWorkout,
 } = require("../controllers/workoutController");
 
 const workoutRouter = express.Router();
 
 workoutRouter.route("/").get(getAllWorkouts);
 
-workoutRouter
-  .route(`/:id`)
-  .get(getWorkoutById)
-  .put(editWorkout)
-  .delete(deleteWorkout);
+// workoutRouter
+//   .route(`/:id`)
+//   .get(getWorkoutById)
+//   .put(editWorkout)
+//   .delete(deleteWorkout);
 
 module.exports = workoutRouter;
