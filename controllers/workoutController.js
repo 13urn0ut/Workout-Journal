a66fe33f170bab42e318d048f3ec10de79e3d870
13urn0ut <arturas.mickevicius@stud.techin.lt>
2025-01-15 10:49:55 +0400

    Add getAllWorkouts to workoutController

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -1,115 +1,23 @@
-const fs = require("fs");
-const path = require("path");
-const Workout = require("./../models/workoutModel");
+const { getAllWorkouts } = require("../models/workoutModel");
 
-const workoutsPath = path.join(__dirname, "../data/workouts.json");
+exports.getAllWorkouts = async (req, res) => {
+  try {
+    const workouts = await getAllWorkouts();
 
-const workouts = JSON.parse(fs.readFileSync(workoutsPath, "utf8"));
-
-workouts.sort((a, b) => a.id - b.id);
-
-exports.getAllWorkouts = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    results: workouts.length,
-    data: workouts,
-  });
-};
-
-exports.addWorkout = (req, res) => {
-  const id = (workouts[workouts.length - 1]?.id || 0) + 1;
-  const userId = +req.params.id;
-  const { name } = req.body;
-
-  const workout = new Workout(id, name, null, userId);
-
-  workouts.push(workout);
-
-  fs.writeFile(workoutsPath, JSON.stringify(workouts), (err) => {
-    if (err)
-      return res.status(500).json({
+    if (!workouts || workouts.length < 1)
+      return res.status(404).json({
         status: "fail",
-        message: "failed to write file",
+        message: "No workouts found",
       });
 
-    res.status(201).json({
+    res.status(200).json({
       status: "success",
-      data: [workout.id],
+      data: workouts,
     });
-  });
-};
-
-exports.getWorkoutById = (req, res) => {
-  const id = +req.params.id;
-  const workout = workouts.find((workout) => workout.id === id);
-
-  if (!workout)
-    return res.status(404).json({
+  } catch (err) {
+    res.status(500).json({
       status: "fail",
-      message: "Workout not found",
+      message: err.message,
     });
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      workout_id: workout.id,
-      workout_name: workout.name,
-    },
-  });
-};
-
-exports.editWorkout = (req, res) => {
-  const id = +req.params.id;
-  const workout = workouts.find((workout) => workout.id === id);
-
-  if (!workout)
-    return res.status(404).json({
-      status: "fail",
-      message: "Workout not found",
-    });
-
-  workout.name = req.body.name;
-
-  fs.writeFile(workoutsPath, JSON.stringify(workouts), (err) => {
-    if (err)
-      return res.status(500).json({
-        status: "fail",
-        message: "failed to write file",
-      });
-
-    res.status(201).json({
-      status: "success",
-      data: Object.keys(req.body).length,
-    });
-  });
-
-  console.log(workouts);
-};
-
-exports.deleteWorkout = (req, res) => {
-  const id = +req.params.id;
-  const workout = workouts.find((workout) => workout.id === id);
-
-  if (!workout)
-    return res.status(404).json({
-      status: "fail",
-      message: "Workout not found",
-    });
-
-  fs.writeFile(
-    workoutsPath,
-    JSON.stringify(workouts.filter((workout) => workout.id !== id)),
-    (err) => {
-      if (err)
-        return res.status(500).json({
-          status: "fail",
-          message: "failed to write file",
-        });
-
-      res.status(204).json({
-        status: "success",
-      });
-      // eslint-disable-next-line prettier/prettier
-    }
-  );
+  }
 };
