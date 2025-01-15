4d08717648071fdaa8f191b42bcb2ce5d3d37cd7
13urn0ut <arturas.mickevicius@stud.techin.lt>
2025-01-15 11:15:35 +0400

    Remove coments

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -6,15 +6,11 @@ const {
   getUserWorkouts,
   addUserWorkout,
 } = require("../controllers/userController");
-// const { addWorkout } = require("../controllers/workoutController");
 const userRouter = express.Router();
 
 userRouter.route("/").get(getAllUsers);
-
 userRouter.route(`/:id`).get(getUserById);
 userRouter.route(`/:username`).get(getUserByUsername);
 userRouter.route(`/:id/workouts`).get(getUserWorkouts).post(addUserWorkout);
-
-// userRouter.route(`/:id/workouts`).post(addWorkout);
 
 module.exports = userRouter;
