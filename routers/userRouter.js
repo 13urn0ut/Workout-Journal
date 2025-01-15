f68443ef4c69697cbd384aefc183dbefccbbbef0
13urn0ut <arturas.mickevicius@stud.techin.lt>
2025-01-15 12:44:00 +0400

    Add checkId to userRouter

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -6,11 +6,16 @@ const {
   getUserWorkouts,
   addUserWorkout,
 } = require("../controllers/userController");
+const { checkId } = require("../middleware/checkId");
 const userRouter = express.Router();
 
 userRouter.route("/").get(getAllUsers);
 userRouter.route(`/:id`).get(getUserById);
 userRouter.route(`/:username`).get(getUserByUsername);
-userRouter.route(`/:id/workouts`).get(getUserWorkouts).post(addUserWorkout);
+userRouter
+  .route(`/:id/workouts`)
+  .all(checkId)
+  .get(getUserWorkouts)
+  .post(addUserWorkout);
 
 module.exports = userRouter;
