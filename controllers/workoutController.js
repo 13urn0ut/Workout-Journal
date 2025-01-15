0bcb50f110c4b320d8ae6b3d6b696ac6196cb3df
13urn0ut <arturas.mickevicius@stud.techin.lt>
2025-01-15 13:29:18 +0400

    Add appErrors to workoutController

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -1,3 +1,4 @@
+const AppError = require("../helpers/appError");
 const {
   getAllWorkouts,
   getWorkoutById,
@@ -10,10 +11,11 @@ exports.getAllWorkouts = async (req, res) => {
     const workouts = await getAllWorkouts();
 
     if (!workouts || workouts.length < 1)
-      return res.status(404).json({
-        status: "fail",
-        message: "No workouts found",
-      });
+      throw new AppError(404, "No workouts found");
+    // return res.status(404).json({
+    //   status: "fail",
+    //   message: "No workouts found",
+    // });
 
     res.status(200).json({
       status: "success",
@@ -33,18 +35,14 @@ exports.getWorkoutById = async (req, res) => {
   try {
     const workout = await getWorkoutById(id);
 
-    if (!workout)
-      return res.status(404).json({
-        status: "fail",
-        message: "Workout not found",
-      });
+    if (!workout) throw new AppError(404, "Workout not found");
 
     res.status(200).json({
       status: "success",
       data: workout,
     });
   } catch (err) {
-    res.status(500).json({
+    res.status(err.status || 500).json({
       status: "fail",
       message: err.message,
     });
@@ -55,21 +53,20 @@ exports.editWorkout = async (req, res) => {
   const { id } = req.params;
   const workoutData = req.body;
 
-  if (Object.keys(workoutData).length < 1)
-    return res.status(400).json({
-      status: "fail",
-      message: "Invalid workout data",
-    });
-
   try {
+    if (Object.keys(workoutData).length < 1)
+      throw new AppError(400, "Invalid workout data");
+
     const workout = await editWorkout(id, workoutData);
+
+    if (!workout) throw new AppError(404, "Workout not found");
 
     res.status(200).json({
       status: "success",
       data: workout,
     });
   } catch (err) {
-    res.status(500).json({
+    res.status(err.status || 500).json({
       status: "fail",
       message: err.message,
     });
@@ -82,11 +79,7 @@ exports.deleteWorkout = async (req, res) => {
   try {
     const deletedWorkout = await deleteWorkout(id);
 
-    if (!deletedWorkout)
-      return res.status(404).json({
-        status: "fail",
-        message: "Workout not found",
-      });
+    if (!deletedWorkout) throw new AppError(404, "Workout not found");
 
     res.status(204).json({
       status: "success",
@@ -94,7 +87,7 @@ exports.deleteWorkout = async (req, res) => {
       data: deletedWorkout,
     });
   } catch (err) {
-    req.status(500).json({
+    res.status(err.status || 500).json({
       status: "fail",
       message: err.message,
     });
