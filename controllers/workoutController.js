d26a57ce0f4ff7d5609fd260ec16e03ee0b082bf
13urn0ut <arturas.mickevicius@stud.techin.lt>
2025-01-15 11:40:00 +0400

    Add editWorkout to workoutController

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -1,4 +1,8 @@
-const { getAllWorkouts, getWorkoutById } = require("../models/workoutModel");
+const {
+  getAllWorkouts,
+  getWorkoutById,
+  editWorkout,
+} = require("../models/workoutModel");
 
 exports.getAllWorkouts = async (req, res) => {
   try {
@@ -39,6 +43,37 @@ exports.getWorkoutById = async (req, res) => {
         status: "fail",
         message: "Workout not found",
       });
+
+    res.status(200).json({
+      status: "success",
+      data: workout,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+
+exports.editWorkout = async (req, res) => {
+  const { id } = req.params;
+  const workoutData = req.body;
+
+  if (!id || isNaN(id))
+    return res.status(400).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+
+  if (Object.keys(workoutData).length < 1)
+    return res.status(400).json({
+      status: "fail",
+      message: "Invalid workout data",
+    });
+
+  try {
+    const workout = await editWorkout(id, workoutData);
 
     res.status(200).json({
       status: "success",
