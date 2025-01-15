56ae79714b2f4bdc433632c655376af50a261dab
13urn0ut <arturas.mickevicius@stud.techin.lt>
2025-01-15 09:52:48 +0400

    Add addUserWorkout to userRouter

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,6 +4,7 @@ const {
   getUserById,
   getUserByUsername,
   getUserWorkouts,
+  addUserWorkout,
 } = require("../controllers/userController");
 // const { addWorkout } = require("../controllers/workoutController");
 const userRouter = express.Router();
@@ -12,7 +13,7 @@ userRouter.route("/").get(getAllUsers);
 
 userRouter.route(`/:id`).get(getUserById);
 userRouter.route(`/:username`).get(getUserByUsername);
-userRouter.route(`/:id/workouts`).get(getUserWorkouts);
+userRouter.route(`/:id/workouts`).get(getUserWorkouts).post(addUserWorkout);
 
 // userRouter.route(`/:id/workouts`).post(addWorkout);
 
